4c12e39e56e928c4fc15818711fbba28b71bd94c
ddrag23 <kurodyami@gmail.com>
2025-07-05 17:51:06 +0700

    dynaimc alembic url

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -12,6 +13,13 @@ sys.path.append('./')
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# Override sqlalchemy.url with environment variable if available
+from dotenv import load_dotenv
+load_dotenv()
+database_url = os.getenv('DATABASE_URL')
+if database_url:
+    config.set_main_option('sqlalchemy.url', database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
